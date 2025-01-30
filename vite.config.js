2218d451fa467bf346ed09d9f82654920e801d5c
g8t0rboi <g8t0rboispam@gmail.com>
2025-01-30 15:56:20 -0500

    Update config to load env variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return {
+    server: {
+      port: process.env.VITE_PORT,
+      watch: {
+        usePolling: true,
+      }
+    },
+    plugins: [react({
+     include: "**/*.jsx",
+    })],
+  }
 })
