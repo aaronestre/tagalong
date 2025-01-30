fe591bd846638a7b36708cc5d5ff44f157c16ec8
g8t0rboi <g8t0rboispam@gmail.com>
2025-01-30 01:19:32 -0500

    Add home page

--- api/hello.jsx
+++
@@ -1,6 +0,0 @@
-export default function handler(request, responsee) {
-  const { name = 'World' } = request.query;
-  response.status(200).json({
-    message: `Hello ${name}!`,
-  });
-}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+        <div>
+            <h1>Welcome Home</h1>
+            <p>Landing page for Tagalong</p>
+        </div>
+    </>
+  );
+}
+export default Home;
