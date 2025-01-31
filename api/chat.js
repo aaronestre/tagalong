61154a44d997b58709b1eda726a3ea4ed15e9f49
g8t0rboi <g8t0rboispam@gmail.com>
2025-01-30 20:42:25 -0500

    Remove uneccessary import

--- api/chat.js
+++ api/chat.js
@@ -1,24 +1,22 @@
 import Groq from "groq-sdk";
-import { config } from "dotenv";
-config();
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-const groq = new Groq({ apiKey: process.env.VITE_GROQ_API_KEY });
-
-export async function getGroqChatCompletion() {
+export async function getGroqChatCompletion(content) {
   return groq.chat.completions.create({
     messages: [
       {
         role: "user",
-        content: "Explain the importance of fast language models",
+        content: content,
       },
     ],
     model: "llama-3.3-70b-versatile",
+    temperature: 0.8
   });
 }
 
 export async function GET(request) {
     try {
-        const chatCompletion = await getGroqChatCompletion();
+        const chatCompletion = await getGroqChatCompletion(request);
         return new Response(chatCompletion.choices[0]?.message?.content || "");
     } catch (error) {
         console.error("Error getting Groq completion:", error);
