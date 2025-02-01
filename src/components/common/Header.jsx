f7be7976f8c7db74a869bb2bd8cb248fe88d018d
g8t0rboi <g8t0rboispam@gmail.com>
2025-02-01 02:51:28 -0500

    Added anchor tag for Header Text to redirect to Home Page

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,38 +1,56 @@
 import React from "react";
 import {
-  Flex,
-  AppShell,
-  Burger,
-  Button,
-  useMantineColorScheme,
-  useComputedColorScheme,
-  Text,
+    Flex,
+    AppShell,
+    Burger,
+    Button,
+    useMantineColorScheme,
+    useComputedColorScheme,
+    Text,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconSun, IconMoon } from "@tabler/icons-react";
 
-function Header({opened, toggle}) { 
-  const { setColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme("light");
+function Header({ opened, toggle }) {
+  
+    const homePage = import.meta.env.VITE_API_URL
+    const { setColorScheme } = useMantineColorScheme();
+    const computedColorScheme = useComputedColorScheme("light");
 
-  const toggleColorScheme = () => {
-    setColorScheme(computedColorScheme === "dark" ? "light" : "dark");
-  };
+    const toggleColorScheme = () => {
+        setColorScheme(computedColorScheme === "dark" ? "light" : "dark");
+    };
 
-  return (
-    <AppShell.Header>
-      <Flex
-        justify={"space-between"}
-        align={"center"}
-        style={{ padding: "10px 20px" }}
-      >
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        <Text size="xl" variant="gradient" gradient={{ from: "orange", to: "red", deg: 45 }}>Tagalong</Text>
-        <Button size="sm" variant="link" onClick={toggleColorScheme}>
-          {computedColorScheme === "dark" ? <IconMoon /> : <IconSun />}
-        </Button>
-      </Flex>
-    </AppShell.Header>
-  );
+    return (
+        <AppShell.Header>
+            <Flex
+                justify={"space-between"}
+                align={"center"}
+                style={{ padding: "10px 20px" }}
+            >
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                />
+                <Text
+                    size="lg"
+                    fw="700"
+                    variant="gradient"
+                    gradient={{ from: "orange", to: "red", deg: 45 }}
+                >
+                    <a href={homePage}>Tagalong</a>
+                </Text>
+                <Button size="sm" variant="link" onClick={toggleColorScheme}>
+                    {computedColorScheme === "dark" ? (
+                        <IconMoon />
+                    ) : (
+                        <IconSun />
+                    )}
+                </Button>
+            </Flex>
+        </AppShell.Header>
+    );
 }
 export default Header;
