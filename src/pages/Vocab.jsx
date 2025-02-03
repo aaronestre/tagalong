f87cda1cf4c46a9bd733129ef163455ac9caebe7
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-03 17:23:20 -0500

    Add functionality to Vocab page, a vocab card that can query a random word from supabase

--- src/pages/Vocab.jsx
+++ src/pages/Vocab.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import {React, useState} from "react";
 import axios from "axios";
-import { Button } from "@mantine/core";
+import { Paper, Text, Center, Loader } from "@mantine/core";
+import Button from "../components/common/Button";
+
+import "../styles/vocab.css";
 
 function Vocab() {
 
+    const [vocabWord, setVocabWord] = useState({});
+    const [loading, setLoading] = useState(false);
+
     const handleOnClick = async () => {
+        setLoading(true);
         try {
             const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/vocab/fetchRandomWord`);
-            console.log(res.data.word);
+            console.log(res.data.word[0]);
+            setVocabWord(res.data.word[0]);
         } catch (error) {
             console.log("Error getting POST response", error);
         }
+        setLoading(false);
     };
 
     return (
-        <>
-            <Button onClick={handleOnClick}>Fetch</Button>
-            Vocab Page
-        </>
+        <div w="100vh" h="100%">
+            <Center m="0 auto">
+                <Paper shadow="0px 0px 20px -4px rgba(0,0,0,0.75)" w="600px" h="300px" m="auto"
+                style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    {loading 
+                        ? <Loader color="orange"/> 
+                        : <Text ta="center" fz="50px">{vocabWord.tagalog}</Text> }
+                          
+                </Paper>
+               
+            </Center>
+            <Button m={"15px auto"} w={"200px"} onClick={handleOnClick} disabled={loading} text="New Word"></Button>
+        </div>
     )
 
 }
