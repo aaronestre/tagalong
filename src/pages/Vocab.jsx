ab952bdcdb5754a463438d23a42caa4a99024896
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-04 20:14:03 -0500

    Add definitions to vocab cards by clicking on them to flip.

--- src/pages/Vocab.jsx
+++ src/pages/Vocab.jsx
@@ -5,13 +5,21 @@ import Button from "../components/common/Button";
 
 import "../styles/vocab.css";
 
-function Vocab() {
+export default function Vocab() {
 
     const [vocabWord, setVocabWord] = useState({});
     const [loading, setLoading] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
-    const handleOnClick = async () => {
+    const handleFlip = () => {
+        setIsFlipped( (prev) => !prev );
+    }
+
+    const handleFetchWord = async () => {
         setLoading(true);
+        if ( isFlipped ) {
+            setIsFlipped(false);
+        }
         try {
             const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/vocab/fetchRandomWord`);
             console.log(res.data.word[0]);
@@ -22,22 +30,30 @@ function Vocab() {
         setLoading(false);
     };
 
+    const handleFlipCard = () => {
+
+
+
+    }
+
     return (
-        <div w="100vh" h="100%">
-            <Center m="0 auto">
+        <>
+            <Center display="block" bd="1px solid black">
                 <Paper shadow="0px 0px 20px -4px rgba(0,0,0,0.75)" w="600px" h="300px" m="auto"
-                style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    {loading 
+                style={{ display: "flex", alignItems: "center", justifyContent: "center"}}
+                onClick={handleFlip}>
+                    {   loading 
                         ? <Loader color="orange"/> 
-                        : <Text ta="center" fz="50px">{vocabWord.tagalog}</Text> }
+                        : <Text ta="center" fz="50px">
+                            {isFlipped ? vocabWord.definition : vocabWord.tagalog}
+                          </Text>
+                    }
                           
                 </Paper>
-               
+                <Button m={"15px auto"} w={"200px"} onClick={handleFetchWord} disabled={loading} text="New Word"></Button>
             </Center>
-            <Button m={"15px auto"} w={"200px"} onClick={handleOnClick} disabled={loading} text="New Word"></Button>
-        </div>
+            
+        </>
     )
 
 }
-
-export default Vocab
