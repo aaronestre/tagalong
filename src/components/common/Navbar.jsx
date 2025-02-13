2145f0eb0b7f72211dd03f79d9d331bd2f4efe7d
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-13 00:24:18 -0500

    Add styling to navbar links to reflect active tab

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import {React, useState} from "react";
 import { AppShell, NavLink, Text } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 import { IconSchool } from "@tabler/icons-react";
 import { IconBook2 } from "@tabler/icons-react";
 import { IconLogout2 } from '@tabler/icons-react';
 
+import "../../styles/navbar.css";
+
 function Navbar() {
   const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("/chat");
+
+  const handleClick = (route) => {
+    navigate(route);
+    setIsActive(route);
+  }
 
   return (
     <AppShell.Navbar p="md" style={{ gap: "10px" }}>
       <AppShell.Section>
         <NavLink
+          className="link"
           leftSection={<IconSchool />}
           label={<Text fz="20px">Tutor</Text>}
-          onClick={() => navigate("/chat")}
-          style={{ margin: "5px" }}
+          onClick={() => handleClick("/chat")}
+          m="5px"
+          bg={isActive === "/chat" ? "orange" : ""}
+          c={isActive === "/chat" ? "white" : ""}
         />
         <NavLink
+          className="link"
           leftSection={<IconBook2 />}
           label={<Text fz="20px">Vocab</Text>}
-          onClick={() => navigate("/vocab")}
-          style={{ margin: "5px" }}
+          onClick={() => handleClick("/vocab")}
+          m="5px"
+          bg={isActive === "/vocab" ? "orange" : ""}
+          c={isActive === "/vocab" ? "white" : ""}
+
         />
       </AppShell.Section>
       <AppShell.Section grow></AppShell.Section>
