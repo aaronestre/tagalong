dd6a85fbc1ee7dae794156526667df414d1f6edf
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-14 01:14:41 -0500

    Switch Supabase key to service key

--- src/api/vocabApi.js
+++ src/api/vocabApi.js
@@ -7,8 +7,8 @@ export const handleFetchWord = async (isFlipped, setIsFlipped, setLoadingWord, s
     }
     try {
         const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/vocab/fetchRandomWord`);
-        console.log(res.data.word[0]);
-        setVocabWord(res.data.word[0]);
+        console.log(res.data.word);
+        setVocabWord(res.data.word);
     } catch (error) {
         console.log("Error getting POST response", error);
     }
@@ -28,8 +28,11 @@ export const handleFetchTextToSpeech = async (vocabWord, setLoadingTTS) => {
 
         const blob = new Blob([res.data], { type: "audio/mpeg" });
         const url = URL.createObjectURL(blob);
+        
         const audio = new Audio(url);
         audio.play();
+
+        const data = await uploadAudioToSupabaseDatabase(vocabWord.tagalog, url);
     }
     catch (error) {
         console.log("Error getting POST response", error);
@@ -38,3 +41,37 @@ export const handleFetchTextToSpeech = async (vocabWord, setLoadingTTS) => {
         setLoadingTTS(false);
     }
 }
+
+// export const uploadAudioToSupabaseStorage = async (blob, word) => { 
+
+//     const fileName = `audio_${word}.mp3`;
+//     const filePath = `tts-audio/${fileName}`;
+
+//     const file = new File([blob], fileName, { type: "audio/mpeg" });
+
+//     const url = await axios.post(`${import.meta.env.VITE_API_URL}/api/vocab/uploadBlob`, {file, filePath});
+//     console.log(url.data);
+//     return url.data;
+
+// }
+
+// export const uploadAudioToSupabaseDatabase = async (word, url) => {
+//     const data = axios.post(`${import.meta.env.VITE_API_URL}/api/vocab/updateAudioURL`, {word, url});
+
+//     return data;
+// }
+
+// export const testUploadingAudio = async () => { 
+//     const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/vocab/textToSpeech`,
+//         {text: "abain"}, 
+//         {responseType: "arraybuffer"}
+//     );
+
+//     const blob = new Blob([res.data], { type: "audio/mpeg" });
+
+//     const url = await uploadAudioToSupabaseStorage(blob, "abain");
+//     const data = uploadAudioToSupabaseDatabase("abain", url);
+
+//     console.log("Uploaded");
+
+// }
